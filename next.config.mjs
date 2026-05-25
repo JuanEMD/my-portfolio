199bed13cec624be4cf9898f9f99af353e59e9b9
JuanEMD <juanernestomarmolejosdanis@gmail.com>
2026-05-25 16:35:44 -0400

    next turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 import i18nConfig from "./next-i18next.config.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   i18n: i18nConfig.i18n,
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
