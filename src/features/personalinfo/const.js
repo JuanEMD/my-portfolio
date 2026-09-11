292f40765397dc4b8083bc57f1f2cd3f920b657b
JuanEMD <juanernestomarmolejosdanis@gmail.com>
2026-09-11 16:36:48 -0400

    feat(animations): add fadeRight keyframe and shared animation constants

---
+++ src/features/personalinfo/const.js
@@ -0,0 +1,14 @@
+//properties: element, identificatorType, animation
+export const targetAnimationElements = [
+  {
+    element: "profile-photo",
+    identificatorType: "id",
+    animation: "animate-fade-right",
+  },
+  { element: "contact-me", identificatorType: "id" },
+  { element: "linkedin", identificatorType: "id" },
+  { element: "github", identificatorType: "id" },
+  { element: "name", identificatorType: "id" },
+  { element: "role", identificatorType: "id" },
+  { element: "description", identificatorType: "id" },
+];
